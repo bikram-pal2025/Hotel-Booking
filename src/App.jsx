7b67374d2c67c7dc83ba028ccdafe87a731f522b
Bikram pal <bikram202121pal@gmail.com>
2026-05-02 08:37:51 +0530

    fix assets.js and add routs

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import BookHotel from './Pages/BookHotel';
+import HotelRoom from './Pages/HotelRoom';
+import MyBooking from './Pages/MyBooking';
+import Exprience from './Pages/Exprience';
 
 const App = () => {
   return (
@@ -9,7 +14,12 @@ const App = () => {
        <Router>
         <Route>
         
-          <Route/>
+          <Route path='/' element={<Home/>} />
+          <Route path='/book-hotel' element={<BookHotel/>} />
+          <Route path='/hotel-room' element={<HotelRoom/>} />
+          <Route path='/my-booking' element={<MyBooking/>} />
+          <Route path='/exprience' element={<Exprience/>} />
+          <Route path='/' element={} />
         </Route>
        </Router>
 
