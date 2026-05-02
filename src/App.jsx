ceecd3cba8f85d967c8e609534fbd13eca31c57f
Bikram pal <bikram202121pal@gmail.com>
2026-05-02 07:27:22 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <div>
+      
+       <Router>
+        <Route>
+          <Route/>
+        </Route>
+       </Router>
+
+
+    </div>
+  )
+}
+
+export default App
