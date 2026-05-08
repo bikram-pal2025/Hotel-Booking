b5da66f0e58c6859d1bd51f217ebfada11868c0a
Bikram pal <bikram202121pal@gmail.com>
2026-05-08 21:38:42 +0530

    add exclosive offer section

--- src/component/ExclosiveOffer.jsx
+++ src/component/ExclosiveOffer.jsx
@@ -3,45 +3,73 @@ import { assets, exclusiveOffers } from "../assets/assets";
 
 const ExclosiveOffer = () => {
   return (
-    <>
-      <div className="flex w-[90%]  md:w-[70%] lg:w-[90%] mx-auto flex-col justify-center items-start mt-10">
-        
-        {/* heading */}
-        <h1 className="font-['Playfair_Display'] text-2xl md:text-2xl lg:text-4xl ">Exclusive Offers</h1>
+    <div className="w-[90%] mx-auto mt-10 mb-24">
 
-        <div className= " mt-4 w-full flex flex-col md:flex-row justify-between items-center">
-            <p className="text-[#778191] w-full md:w-full lg:w-[50%] ">Take advantage of our limited-time offers and special packages to enhance your stay and create unforgettable memories.</p>
+      {/* Heading */}
+      <h1 className="font-['Playfair_Display'] text-3xl lg:text-4xl">
+        Exclusive Offers
+      </h1>
 
-            <div className=" group cursor-pointer flex gap-2">
-                <p className=" font-medium " >View All Offers</p>
-                <img className=" group-hover:translate-x-1 duration-500 " src={assets.arrowIcon} alt="" />
-            </div>
+      {/* Description + Button */}
+      <div className="mt-4 flex flex-col md:flex-row md:items-center md:justify-between gap-4">
 
+        <p className="text-[#778191] md:w-[70%] lg:w-[50%]">
+          Take advantage of our limited-time offers and special packages
+          to enhance your stay and create unforgettable memories.
+        </p>
 
+        <div className="group cursor-pointer flex items-center gap-2">
+          <p className="font-medium">View All Offers</p>
 
-            
+          <img
+            className="group-hover:translate-x-1 duration-300"
+            src={assets.arrowIcon}
+            alt=""
+          />
         </div>
-
-        {/* offer Card */}
-
-        <div className="flex flex-wrap justify-center items-center">
-
-       {
-        exclusiveOffers.map((item)=>(
-            <div className=" w-[350px] h-[100px] bg-red rounded-md " key={item._id}>
-
-      
-
-
-
-            </div>
-        ))
-       }
-
-        </div>
-
       </div>
-    </>
+
+      {/* Offer Cards */}
+      <div className="  mt-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+
+        {exclusiveOffers.map((item) => (
+          <div
+            key={item._id}
+            className="relative group  h-[250px] rounded-xl bg-cover bg-center bg-no-repeat p-5 flex flex-col justify-start"
+            style={{ backgroundImage: `url(${item.image})` }}
+          >
+            {/* Discount */}
+            <p className="w-fit px-3 py-1 bg-white rounded-full text-xs font-medium">
+              {item.priceOff}% OFF
+            </p>
+
+            {/* Title */}
+            <h3 className="mt-4 text-white text-2xl font-['Playfair_Display']">
+              {item.title}
+            </h3>
+         
+            {/* description */}
+            <p className="text-white font-medium" >{item.description} </p>
+
+            {/* expiryDate */}
+
+            <p className="text-[#C6D3D5] mt-3 text-xs font-medium">Expires {item.expiryDate} </p>
+
+            <div className=" absolute bottom-[20px] text-white cursor-pointer flex items-center gap-2">
+          <p className="font-medium">View All Offers</p>
+
+          <img
+            className="group-hover:translate-x-1 duration-300 brightness-0 invert  "
+            src={assets.arrowIcon}
+            alt=""
+          />
+        </div>
+          </div>
+
+          
+        ))}
+      </div>
+    </div>
   );
 };
 
